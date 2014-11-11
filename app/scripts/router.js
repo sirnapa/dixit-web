e8b0f58e20b33bfc8d566ba4be5dc074615728fd
Nelson Antonino Paez <nelpa90@gmail.com>
2014-11-11 01:31:36 -0300

    Sección de Ideas y Recicla.me

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,4 +2,13 @@ Dixitweb.Router.map(function () {
   this.resource('blogs');
   this.resource('nosotros');
   this.resource('trabajos');
+  this.resource('ideas',function(){
+		this.resource('ideas.reciclame',{ path: '/reciclame' },function(){
+	      this.route('problematica');
+	      this.route('comunidad');
+	      this.route('propuesta');
+	      this.route('objetivo');
+	      this.route('tecnologia');
+	    });
+	});
 });
