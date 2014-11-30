1fbbab1dbec23635d0bd943c09bde4989125ddb5
Nelson Antonino Paez <nelpa90@gmail.com>
2014-11-30 17:14:55 -0300

    Correcciones de Index y Blogs

---
+++ app/scripts/routes/blogs_route.js
@@ -0,0 +1,29 @@
+Dixitweb.BlogsRoute = Ember.Route.extend({
+    model: function () {
+        return [{
+        	nombre: 'Funnix',
+            sitio: 'funnix.net',
+            logo: 'funnix.svg',
+        	facebook: 'funnixblog',
+            twitter: 'funnixblog'
+        },{
+        	nombre: 'Un blog... de onda',
+            sitio: 'blogdeonda.com',
+            logo: 'blogdeonda.png',
+            facebook: 'blogdeonda',
+            twitter: 'blogdeonda'
+        },{
+        	nombre: 'RocknTown',
+            sitio: 'rockntown.com',
+            logo: 'rockntown.svg',
+            facebook: 'rockntown',
+            twitter: 'rockntown'
+        },{
+        	nombre: 'Akasha blog',
+            sitio: 'akashablog.net',
+            logo: 'akashablog.png',
+            facebook: 'akashablog',
+            twitter: 'akashablog'
+        }];
+    }
+});
