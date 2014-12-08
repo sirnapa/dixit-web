99a7abce08dd04d77db8c722e50de150ad48ed64
Nelson Antonino Paez <nelpa90@gmail.com>
2014-12-07 23:48:44 -0300

    Tutorial para agregar videos y ejemplo de post.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -15,8 +15,10 @@ Dixitweb.Router.map(function () {
   this.resource('blogs');
   this.resource('ghost',function(){
 	this.route('post');
+	this.route('ejemplo-post');
 	this.route('formato');
 	this.route('imagenes');
+	this.route('videos');
 	this.route('botones');
 	this.route('perfil');
 	this.route('cambiar-clave');
