80ac94f83bdaa85d43ef9b213c1f68f17ce0e63e
Nelson Antonino Paez <nelpa90@gmail.com>
2016-05-01 18:48:40 -0400

    Removidas las secciones de equipo, trabajos e ideas.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,17 +1,5 @@
 Dixitweb.Router.map(function () {
   this.resource('nosotros');
-  this.resource('equipo');
-  this.resource('ideas',function(){
-		this.resource('ideas.reciclame',{ path: '/reciclame' },function(){
-	      this.route('problematica');
-	      this.route('comunidad');
-	      this.route('propuesta');
-	      this.route('objetivo');
-	      this.route('tecnologia');
-	      this.route('premios-conecta');
-	    });
-	});
-  this.resource('trabajos');
   this.resource('blogs');
   this.resource('ghost',function(){
 	this.route('post');
